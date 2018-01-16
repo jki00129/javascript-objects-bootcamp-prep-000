6b24b72dc06b5afdbc3d30d9322172f422e6755a
Jeewon Kim <jki00129@gmail.com>
2018-01-16 02:28:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,4 +2,5 @@ var playlist = {'Jay Z': 'Monster'};
 function updatePlaylist(object, key, value){
   return Object.assign({}, object, { [key]: value})
 }
-
+function removeFromPlaylist(playlist, artistName){
+  return 
